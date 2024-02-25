de4545c3cc2b47d7117336a9eb9532ee3a750565
Andre <acardoso.ny@gmail.com>
2024-02-25 11:43:07 +0000

    deploying

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/newestsite/'
+    ? '/Acardoso21.github.io/'
     : '/',
   transpileDependencies: true,
   pluginOptions: {
