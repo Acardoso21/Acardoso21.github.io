da4c517b45aa1f4920058a804bc454ff4f498ebd
Andre <102825692+Acardoso21@users.noreply.github.com>
2024-02-25 11:58:43 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<Acardoso21>/<Acardoso21>.github.io.git main
+git push -f git@github.com:Acardoso21/Acardoso21.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<Acardoso21>/<Acardoso21>.git main:gh-pages
+git push -f git@github.com:Acardoso21/Acardoso21.git main:gh-pages
 
 cd -
