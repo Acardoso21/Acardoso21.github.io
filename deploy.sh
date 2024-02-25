b34f0bf20e07f7a61797b8c8170bad2806950510
Andre <acardoso.ny@gmail.com>
2024-02-25 13:57:26 +0000

    c

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Acardoso21/Acardoso21.github.io.git main
+# git push -f git@github.com:Acardoso21/Acardoso21.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Acardoso21/Acardoso21.git main:gh-pages
