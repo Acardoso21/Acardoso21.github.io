3238da2a9b2c0d768fc5fd788530e8812288e756
Andre <acardoso.ny@gmail.com>
2024-02-25 13:56:39 +0000

    changes

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/andrecardoso.me/'
+    ? '/acardoso21.github.io/'
     : '/',
     base
   transpileDependencies: true,
